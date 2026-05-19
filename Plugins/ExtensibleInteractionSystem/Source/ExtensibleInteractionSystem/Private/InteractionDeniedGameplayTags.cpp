43839589a246c7691fa6cab1804f0e11a7a52062
linujern <linus@braincurrent.se>
2026-05-19 21:01:17 +0200

    Gameplay Tags standardized

--- Plugins/ExtensibleInteractionSystem/Source/ExtensibleInteractionSystem/Private/InteractionDeniedGameplayTags.cpp
+++ Plugins/ExtensibleInteractionSystem/Source/ExtensibleInteractionSystem/Private/InteractionDeniedGameplayTags.cpp
@@ -1,8 +1,11 @@
 #include "InteractionDeniedGameplayTags.h"
 
+UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_None, "Interaction.Denied.None")
+UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_Unknown, "Interaction.Denied).Unknown")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_NotFocusable, "Interaction.Denied.NotFocusable")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_NotInteractable, "Interaction.Denied.NotInteractable")
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_AlreadyInProgress, "Interaction.Denied.AlreadyInProgress")
+
 UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_Cooldown_Global, "Interaction.Denied.Cooldown.Global")
 UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_Cooldown_PerPlayer, "Interaction.Denied.Cooldown.PerPlayer")
-UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_AlreadyInProgress, "Interaction.Denied.AlreadyInProgress")
-UE_DEFINE_GAMEPLAY_TAG(TAG_Interaction_Denied_OutOfRange, "Interaction.Denied.OutOfRange")
